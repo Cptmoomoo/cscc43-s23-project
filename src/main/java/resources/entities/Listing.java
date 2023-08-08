0e90faedb4d36f838399fced8c979e4df5a6d638
Vincent Li <vinchenzoli3@gmail.com>
2023-08-08 06:11:10 -0400

    listing format changes

--- src/main/java/resources/entities/Listing.java
+++ src/main/java/resources/entities/Listing.java
@@ -53,10 +53,10 @@ public class Listing
             location2 = new Location((float) 0, (float) 0, "", "", "", "", "", "");
 
         // Need to print nice format, with nice format of location
-        if (suiteNum != null)
-            return String.format("%s (%s) %s\nAverage price per day: $%.2f\n%s", listingType, listingID, amenities.toString(), avgPrice, location2.toString());
+        if (suiteNum != null && !suiteNum.isBlank())
+            return String.format("%s (%s) %s\nAverage price per day: $%.2f\nMax guests: %d\nSuite: %s\n %s", listingType, listingID, amenities.toString(), avgPrice, maxGuests, suiteNum, location2.toString());
         
-        return String.format("%s %s (%s) %s\n%s", suiteNum, listingType, listingID, amenities.toString(), location2.toString());
+        return String.format("%s (%s) %s\nAverage price per day: $%.2f\nMax guests: %d\n %s", listingType, listingID, amenities.toString(), avgPrice, maxGuests, location2.toString());
     }
 
 }
